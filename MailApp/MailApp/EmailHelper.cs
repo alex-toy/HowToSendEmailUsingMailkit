16e3f97e71b7794d528184ac5bf2addcd6e2c6e5
alex-toy <alexei.80@hotmail.fr>
2023-02-25 11:27:35 +0100

    init

---
+++ MailApp/MailApp/EmailHelper.cs
@@ -0,0 +1,72 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System;
+
+namespace MailApp
+{
+    public class EmailHelper
+    {
+        public string Sender { get; set; }
+        public string Name { get; set; }
+        public string Receiver { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public TextPart Body { get; set; }
+
+        public EmailHelper(string sender, string name, string receiver, string subject, string message)
+        {
+            this.Sender = sender;
+            this.Name = name;
+            this.Receiver = receiver;
+            this.Subject = subject;
+            this.Message = message;
+            this.Body = new TextPart("plain")
+            {
+                Text = message
+            };
+
+        }
+
+        public MimeMessage PrepareMessage()
+        {
+            MimeMessage message = new MimeMessage();
+            MailboxAddress mailBoxAddress = new MailboxAddress(Name, Sender);
+
+            message.From.Add(mailBoxAddress);
+
+            MailboxAddress temp = MailboxAddress.Parse(Receiver);
+            message.To.Add(temp);
+
+            message.Subject = Subject;
+
+            message.Body = Body;
+
+            return message;
+        }
+
+        public void SendMail(string password)
+        {
+            SmtpClient smtpClient = new SmtpClient();
+
+            try
+            {
+                smtpClient.Connect("smtp.gmail.com", 465, true);
+                smtpClient.Authenticate(Sender, password);
+                MimeMessage mimeMessage = PrepareMessage();
+                smtpClient.Send(mimeMessage);
+                Console.WriteLine("email sent!");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+            finally
+            {
+                smtpClient.Disconnect(true);
+                smtpClient.Dispose();
+            }
+        }
+    }
+}
---
+++ MailApp/MailApp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using MimeKit;
+using System;
+
+namespace MailApp
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sender : ");
+            string sender = Console.ReadLine();
+
+            Console.Write("password : ");
+            string password = Console.ReadLine();
+
+            Console.Write("receiver : ");
+            string receiver = Console.ReadLine();
+
+            string name = "me";
+            string subject = "test";
+            string message = "Hello my friend!";
+
+            EmailHelper emailHelper = new EmailHelper(sender, name, receiver, subject, message);
+            emailHelper.SendMail(password);
+
+        }
+    }
+}
